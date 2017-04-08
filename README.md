f450bf8ff53e88e7832679d428325405a987f57c
Caitlyn Zahn <caitlyn.zahn@gmail.com>
2017-04-08 19:58:15 -0400

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# Resume Prokect
+[GitHub Link](https://github.com/createlyn/project_resume_zahn_caitlyn)
 
+
+### Resources
+[id Tabs](http://www.sunsean.com/idTabs/#t2)
+This was used to create the tabs to show each section of information.
+
+[text rotator](https://github.com/peachananr/simple-text-rotator)
+This was used to rotate the text on the skills section.
+
+### Deductions
+>"I reviewed the list of deductions for this project and in the syllabus."
+
+### Comments
+Sorry again that this is late!
--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+/* this is tabs */
  $('#resume-layout ul').idTabs();
 
+
+/* this is text rotator */
  $('.rotate').textrotator({
      animation: 'dissolve', // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
      separator: ',', // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
      speed: 2000 // How many milliseconds until the next word show.
  });
+
+ /* this is jquery hover effect on info section*/
 
  $('table').hover(
    function () {
